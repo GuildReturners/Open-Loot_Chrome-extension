a8d15666e4712ec675b7b440fe9694754452e69b
Jeremie <jeremie.olivier.pro@gmail.com>
2022-10-13 03:48:20 +0200

    cleanup + add comments + add CSV export

--- src/contents/myStats.tsx
+++ src/contents/myStats.tsx
@@ -1,193 +1,206 @@
-import { FC, useMemo, useReducer } from "react"
-
+import React from "react";
+import { FC, useMemo, useReducer, useState } from "react"
+import type { PlasmoContentScript, PlasmoGetInlineAnchor } from "plasmo";
 
 import type { PurchasedItem, Purchases } from "./interfaces/purchase"
-import type { PlasmoContentScript, PlasmoGetInlineAnchor } from "plasmo";
-import MaterialReactTable, { MRT_ColumnDef, MRT_Row } from 'material-react-table';
-import { Box, Divider, Stack } from '@mui/material';
-
-
 // import { purchasesData } from "./mocks/purchases";
+import MaterialReactTable, { MRT_ColumnDef, MRT_Row } from 'material-react-table';
+import { ExportToCsv } from 'export-to-csv'; 
 
 import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
-import Button from "@mui/material/Button"
-import Input from "@mui/material/Input"
-import Link from "@mui/material/Link"
-import Popover from '@mui/material/Popover';
-import Typography from "@mui/material/Typography"
-import { useState } from "react"
-import React from "react";
+import { Box, Divider, Stack, Button, Input, Link, Popover, Typography } from '@mui/material';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
+// Configure on which page this component will be mounted
+export const config: PlasmoContentScript = {
+  matches : [ "https://openloot.com/my-stats"]
+}
+
+// Configure under which element of the page this component will be mounted
+export const getInlineAnchor = () => {
+  return document.querySelector("header")
+}
+
+// Inject style from without the component shadow root
 const styleElement = document.createElement("style")
-
 const styleCache = createCache({
   key: "plasmo-mui-cache",
   prepend: true,
   container: styleElement
 })
-
 export const getStyle = () => styleElement
 
-// document.querySelector("header").appendChild(styleElement)
-
-
-
-export const config: PlasmoContentScript = {
-  matches : [ "https://openloot.com/my-stats"]
-}
-
-// export const getInlineAnchor : 
-// PlasmoGetInlineAnchor = () =>  
-//   document.querySelector("main")
-
-
-  import type { PlasmoGetOverlayAnchor } from "plasmo"
- 
-
-
-  export const getInlineAnchor = () => {
-    return document.querySelector("header")
-  }
-
-
-
+// QUICK FIX : the MUI popover is not injected within the component shadow root, so the injected style above this liine is not applied to it.
+// TODO : 
+//   - Find a way to force to MUI Popover component to be injected in the component shadow root
+//   - Or inject all the style directly into the webpage 
 function fixPopover(){
-
   const popoverStyle = document.createElement("style")
   popoverStyle.innerText = `.MuiPopover-root {background-color: white;}svg.MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium.plasmo-mui-cache-i4bv87-MuiSvgIcon-root {height: 25px;}.MuiBox-root.plasmo-mui-cache-70qvj9 {display: flex;color: black;cursor: pointer;margin-bottom: 10px;}`;
   document.querySelector("body").appendChild(popoverStyle)
- 
 }
 
-function MyStats() {
-  // loadPurchase()
+
+// As we can't apply 2 aggregation function to a same column we need to define our own so the price aggregation row can show up the both the average and total price
+function getAvgPrice(items) {
+  return items.map( i => i.original.price).reduce( (acc, data) => (acc + data) ) /items.length
+}    
+
+
+
+function UserStats() {
+
   let purchasesData:Purchases = JSON.parse( localStorage['purchases'])
 
   fixPopover()
+
+  // Remove the error page
   document.querySelector("main .chakra-container").innerHTML = "";
 
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
 
-
-    function getAvgPrice(items) {
-      // return items.length
-       return items.map( i => i.original.price).reduce( (acc, data) => (acc + data) ) /items.length
-    }    
-    function getSumPrice(items) {
-      return items.map( i => i.original.price).reduce( (acc, data) => (acc + data)) 
-    }
-
-    const columns = useMemo<MRT_ColumnDef<PurchasedItem>[]>(
-      () => [
-        {
-          header: 'Item',
-          accessorKey: 'item.name',
-          enableGrouping: true, //don't let this column be grouped
-        },
-        {
-          header: 'Price',
-          accessorKey: 'price',
-          //required to render an aggregated cell, show the average salary in the group
-          AggregatedCell: ({ cell, table, row, column }) => (
-            <Stack
-              direction="row"
-              divider={<Divider orientation="horizontal" flexItem />}
-              spacing={2}
-            >
-              <Stack>
-                Avg :
-                <Box color="success.main">{getAvgPrice(row.getLeafRows())?.toLocaleString?.('en-US', {
-                style: 'currency',
-                currency: 'USD',
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 2,
-                })}</Box>
-              </Stack>
-              <Stack>
-                Total:
-                <Box color="success.main">{cell.getValue<number>()?.toLocaleString?.('en-US', {
-                style: 'currency',
-                currency: 'USD',
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-                })}</Box>
-              </Stack>
+  const columns = useMemo<MRT_ColumnDef<PurchasedItem>[]>(
+    () => [
+      {
+        header: 'Item',
+        accessorKey: 'item.name',
+        enableGrouping: true, //don't let this column be grouped
+      },
+      {
+        header: 'Price',
+        accessorKey: 'price',
+        //required to render an aggregated cell, show the average salary in the group
+        AggregatedCell: ({ cell, table, row, column }) => (
+          <Stack
+            direction="row"
+            divider={<Divider orientation="horizontal" flexItem />}
+            spacing={2}
+          >
+            <Stack>
+              Avg :
+              <Box color="success.main">{getAvgPrice(row.getLeafRows())?.toLocaleString?.('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 2,
+              })}</Box>
             </Stack>
-          ),
-        },            
-        {
-          header: 'Id',
-          accessorKey: 'item.issuedId',
-        },
-        {
-          header: 'Purchase Date',
-          accessorKey: 'createdAt',
-        },
-        {
-          header: 'Rarity',
-          accessorKey: 'item.rarity',
-        },
-        {
-          header: 'Status',
-          accessorKey: 'item.status',
-        },      
-        {
-          header: 'openlootid',
-          accessorKey: 'item.id',
-        },   
-        {
-          header: 'Seller',
-          accessorKey: 'createdBy',
-        },     
-        
-      ],
-      [],
-    );
+            <Stack>
+              Total:
+              <Box color="success.main">{cell.getValue<number>()?.toLocaleString?.('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+              })}</Box>
+            </Stack>
+          </Stack>
+        ),
+      },            
+      {
+        header: 'Id',
+        accessorKey: 'item.issuedId',
+      },
+      {
+        header: 'Purchase Date',
+        accessorKey: 'createdAt',
+      },
+      {
+        header: 'Rarity',
+        accessorKey: 'item.rarity',
+      },
+      {
+        header: 'Status',
+        accessorKey: 'item.status',
+      },      
+      {
+        header: 'openlootid',
+        accessorKey: 'item.id',
+      },   
+      {
+        header: 'Seller',
+        accessorKey: 'createdBy',
+      },     
+      
+    ],
+    [],
+   );
+   
+   
+   const csvOptions = {
+     fieldSeparator: ',',
+     quoteStrings: '"',
+     decimalSeparator: '.',
+     showLabels: true,
+     useBom: true,
+     useKeysAsHeaders: false,
+     headers: columns.map((c) => c.header),
+   };
+   const csvExporter = new ExportToCsv(csvOptions);
+   
+   
 
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const handleExportRows = (rows: MRT_Row<PurchasedItem>[]) => {
+    csvExporter.generateCsv(rows.map((row) => row.original));
+  };
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-  
-    return (
-      <CacheProvider value={styleCache}>
-        <MaterialReactTable
-          columns={columns}
-          data={purchasesData.items}
-          enableGrouping
-          enableStickyHeader
-          enableStickyFooter
-          initialState={{
-            columnVisibility: { "item.id": false },
-            density: 'compact',
-            grouping: ['item.name'],
-            pagination: { pageIndex: 0, pageSize: 200 },
-            sorting: [{ id: 'price', desc: false }], //sort by state by default
-          }}
-          muiToolbarAlertBannerChipProps={{ color: 'primary' }}
-          muiTableContainerProps={{ sx: { maxHeight: 500 } }}
-          positionActionsColumn="last"
-          enableRowActions
-          renderRowActions={({ row }) => (
-            <Box sx={{ display: 'flex', gap: '1rem' }}>
-              <a href={"/account/items/" + row.renderValue("item.id")} target="_blank">
-              <Button>Sell</Button>
-              </a>
-            </Box>
-          )}
-        />
-      </CacheProvider>
-    );
-  
-  
+  return (
+    <CacheProvider value={styleCache}>
+      <MaterialReactTable
+        columns={columns}
+        data={purchasesData.items}
+        enableGrouping
+        enableStickyHeader
+        enableStickyFooter
+        initialState={{
+          columnVisibility: { "item.id": false },
+          density: 'compact',
+          grouping: ['item.name'],
+          pagination: { pageIndex: 0, pageSize: 200 },
+          sorting: [{ id: 'price', desc: false }], //sort by state by default
+        }}
+        muiToolbarAlertBannerChipProps={{ color: 'primary' }}
+        muiTableContainerProps={{ sx: { maxHeight: 500 } }}
+        positionActionsColumn="last"
+        enableRowActions
+        renderRowActions={({ row }) => (
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <a href={"/account/items/" + row.renderValue("item.id")} target="_blank">
+            <Button>Sell</Button>
+            </a>
+          </Box>
+        )}
+        positionToolbarAlertBanner="bottom"
+        renderTopToolbarCustomActions={({ table }) => (
+          <Box
+            sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}
+          >
+            <Button
+              disabled={table.getPrePaginationRowModel().rows.length === 0}
+              //export all rows, including from the next page, (still respects filtering and sorting)
+              onClick={() =>
+                handleExportRows(table.getPrePaginationRowModel().rows)
+              }
+              startIcon={<FileDownloadIcon />}
+              variant="contained"
+            >
+              Export All
+            </Button>
+            <Button
+              disabled={table.getRowModel().rows.length === 0}
+              //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
+              onClick={() => handleExportRows(table.getRowModel().rows)}
+              startIcon={<FileDownloadIcon />}
+              variant="contained"
+            >
+              Export Current View
+            </Button>
+          </Box>
+        )}
+      />
+    </CacheProvider>
+  );
 }
   
-export default MyStats;
+export default UserStats;
