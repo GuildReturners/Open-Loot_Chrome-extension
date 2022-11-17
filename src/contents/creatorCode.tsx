7466e1c962307cab41a3ddf46b1d4419ebe21ea9
Jeremie <jeremie.olivier.pro@gmail.com>
2022-11-17 05:45:43 +0100

    Version 1 : dashboard + ingame video + data table

--- src/contents/creatorCode.tsx
+++ src/contents/creatorCode.tsx
@@ -1,18 +1,24 @@
-export {}
+import type { PlasmoContentScript } from "plasmo";
 
-
+export const config: PlasmoContentScript = {
+    matches: ["https://openloot.com/*"],
+  }
+  
           
 
 let previousUrl = '';
 const observer = new MutationObserver(function(mutations) {
-if (location.href !== previousUrl) {
+    if (location.href !== previousUrl) {
     previousUrl = location.href;
     console.log(`URL changed to ${location.href}`);
     if ( location.href.includes('https://openloot.com/checkout?orderId')) injectCreatorCode()
+    if ( location.href.includes('https://openloot.com/items/BT0/')) injectVideo()
     }
 });
-const config = {subtree: true, childList: true};
-observer.observe(document, config);
+const configObs = {
+    subtree: true, childList: true};
+observer.observe(document, configObs);
+
 
 
 
@@ -30,3 +36,17 @@ function injectCreatorCode(){
 
     },1500)
 }
+
+
+function injectVideo(){
+    console.log('in inject video');
+    setTimeout(()=>{  
+        let videoContainer  = document.createElement('div')
+        videoContainer.id = "video-container"
+        document.querySelector('main .chakra-container ').appendChild(videoContainer)
+        document.querySelector('#video-container').innerHTML = '<br><p>A video showcasing how we could have an in-game preview for <b>each NFT</b></p> <iframe width="560" height="315" src="https://www.youtube.com/embed/oGnnoJ76ru8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>'
+  
+  
+    },1500)
+}
+export {}
